a03b155af790f31dfbdb854aa5440a05965e99a5
Wilson Silva <wilson.dsigns@gmail.com>
2021-02-19 22:41:11 +0000

    Initial commit

---
+++ lib/activemodel/extra_validations.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "extra_validations/version"
+
+module Activemodel
+  module ExtraValidations
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/activemodel/extra_validations/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Activemodel
+  module ExtraValidations
+    VERSION = "0.1.0"
+  end
+end
