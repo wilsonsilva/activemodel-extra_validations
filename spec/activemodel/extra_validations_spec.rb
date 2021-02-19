47156a064262732477b84b504020939436f9379d
Wilson Silva <wilson.dsigns@gmail.com>
2021-02-19 22:54:00 +0000

    Remove a dummy spec

--- spec/activemodel/extra_validations_spec.rb
+++ spec/activemodel/extra_validations_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Activemodel::ExtraValidations do
   it "has a version number" do
     expect(Activemodel::ExtraValidations::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
